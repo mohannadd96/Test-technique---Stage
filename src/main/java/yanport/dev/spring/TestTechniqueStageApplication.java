206cbac197da3a00c1a883377a408db5d3d3bc8c
mohannadd96 <mohannad.khadhraoui@esprit.tn>
2021-11-12 15:26:50 +0100

    Init
    initialisation

---
+++ src/main/java/yanport/dev/spring/TestTechniqueStageApplication.java
@@ -0,0 +1,13 @@
+package yanport.dev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechniqueStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechniqueStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yanport/dev/spring/TestTechniqueStageApplicationTests.java
@@ -0,0 +1,13 @@
+package yanport.dev.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechniqueStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
